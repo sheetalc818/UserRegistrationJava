78ddc6f6d41ce8b442890757a86611c79cd910e6
sheetalc818 <sheetalc818@gmail.com>
2021-01-22 12:54:14 +0530

    ADDED : Code to check valid firstname

--- src/main/java/com/bridgelabz/user/UserRegistration.java
+++ src/main/java/com/bridgelabz/user/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.bridgelabz.user;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("*****Welcome to User registration Problem*****");
+        final String REGEX_NAME = "[A-Z]{1}[a-z]{2,}";
+        String firstName = "Bridgelabz";
+        validateFirstName(firstName,REGEX_NAME);
+    }
+    public static void validateFirstName(String firstName, String regex)
+    {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean b = matcher.matches();
+        if (b == true){
+            System.out.println("Pattern Matched, You have Entered " +
+                    "Correct name");
+        }
+        else{ System.out.println("Please enter valid firstname!!"); }
     }
 }
+
 
